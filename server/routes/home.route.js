e914db7e90fdec51b67e147fb94b4ca1cc653d04
Mohsen Dorparasti <dorparasti@gmail.com>
2023-09-23 11:59:24 +0330

    [FIX] Fix undefined app.locals.siteSettings When app.locals.siteSettings is not populated for any reason, redirect visitor to /posts page

--- server/routes/home.route.js
+++ server/routes/home.route.js
@@ -6,7 +6,7 @@ const postsController = require("../controllers/posts.controller");
 const route = express.Router();
 
 route.get("/", (req, res) => {
-  return req.app.locals.siteSettings.landingPage
+  return req.app.locals.siteSettings?.landingPage
     ? aboutController.getInfo(req, res)
     : postsController.getPosts(req, res);
 });
